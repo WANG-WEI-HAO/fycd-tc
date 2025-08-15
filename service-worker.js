e7a5670abd1cf2ade5f08fbb538e60962920c96c
王煒豪 <67509874+WANG-WEI-HAO@users.noreply.github.com>
2025-08-16 03:23:01 +0800

    Update service-worker.js
    Signed-off-by: 王煒豪 <67509874+WANG-WEI-HAO@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,21 @@
+// 設定版本號
+const VERSION = 'v1.1.0';
 
 self.addEventListener('install', event => {
-  console.log('Service worker installing...');
+  console.log(`Service worker installing... [${VERSION}]`);
+  // 立即啟用新版本，不等舊版本被關閉
   self.skipWaiting();
 });
+
 self.addEventListener('activate', event => {
-  console.log('Service worker activated');
+  console.log(`Service worker activated [${VERSION}]`);
+  // 強制接管所有頁面
+  event.waitUntil(
+    clients.claim()
+  );
 });
+
 self.addEventListener('fetch', event => {
+  // 直接從網路抓取，保證最新資源
   event.respondWith(fetch(event.request));
 });
